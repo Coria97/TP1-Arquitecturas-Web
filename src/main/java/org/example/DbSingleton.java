ab2928ae35e29aff67bffeec3698c1b91b7268f3
Matias Gatti <matiasgatti2301@gmail.com>
2024-09-11 16:06:44 -0300

    Se modifico el nombre a la base de datos

--- src/main/java/org/example/DbSingleton.java
+++ src/main/java/org/example/DbSingleton.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbSingleton {
-    private static final String url = "jdbc:mysql://localhost:3308/db_arquitectura";
+    private static final String url = "jdbc:mysql://localhost:3308/integrador_1";
     private static final String username = "root";
     private static final String password = "";
     private static Connection conn;
